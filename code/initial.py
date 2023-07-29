85e9ec7b465e8104a58cd5f3fc072a823ecaa632
mankiddyman <Aaryan.bhatia20@gmail.com>
2023-07-29 15:42:37 +0100

    able to filter matches for 5 member prescence

--- code/initial.py
+++ code/initial.py
@@ -36,10 +36,11 @@ match_ids=[]
 dates=[]
 n_members_present=[]
 times=[]
+source_player=[]
 #loop through each player and find the matches they played
 for ID in player_ids:
     n_matches=len(client.get_player_matches(ID))
-    for match in range(0,len(str(n_matches))):
+    for match in range(0,int(str(n_matches))):
         match_ids.append(client.get_player_matches(ID)[match]["match_id"])
         #time is in epoch time
         #but how do we know the timezone?
@@ -47,16 +48,54 @@ for ID in player_ids:
         date,time=(strftime('%Y-%m-%d %H:%M:%S', localtime(start_time))).split(" ")
         dates.append(date)
         times.append(time)
-        
+        source_player.append(players[player_ids.index(ID)])
         #identify matches where at least 5 moxie members are present
+#going to add the match ids and dates to the dataframe
 
-matches_df=matches_df.with_columns(pl.Series("Game_ID",match_ids),pl.Series("Date",dates),pl.Series("time",times))
+#increase the size of the dataframe 
+dfs=[matches_df]*len(match_ids)
+matches_df=pl.concat(dfs)
+
+col=pl.Series("Game_ID",match_ids),pl.Series("Date",dates),pl.Series("time",times),pl.Series("source_player",source_player)
+
+matches_df=matches_df.with_columns(col)
 
 
+#now removing duplicates and filling in n_present
+#identify rows with count >=5
+match_counts=matches_df['Game_ID'].value_counts()
+
+matches=pl.DataFrame()
+print("Total Unique match ids",len(match_counts))
+for i in range(0,len(match_counts)):
+    #if the count is >5 record the match id
+   
+          
+
+    if match_counts.row(i)[1]>=5:
+        print("size_before",len(matches))    
+
+        #find index of the row containing match id in matches_df
+        id=match_counts.row(i)[0]        
+        
+        matches=pl.concat([matches,matches_df.filter(pl.col("Game_ID")==id)])
+
+        print("size_after",len(matches))
 #parse the data and add to df
 
+matches_df=matches
 
+for i in range(0,len(matches_df)):
+    df['Game_#'].append(i+1)
+    df['Game_ID'].append(matches_df['Game_ID'][i])
+    df['Date'].append(matches_df['Date'][i])
+    
+    match=client.get_match(matches_df['Game_ID'][i])
+    
+    players=match['players']
+#here
 
+    df['Radiant_or_Dire']
 
 
 # do these guys think they are good..?
